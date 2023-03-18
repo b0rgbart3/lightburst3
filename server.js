52d3b48f66d888244f4fc325982ad06c29398412
Bart Dority <b0rgBart3@gmail.com>
2023-03-18 09:35:23 -0700

    adding express

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const path = require("path");
 const PORT = process.env.PORT || 3002;
 const app = express();
 
-const routes = require("./routes");
+// const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+// if (process.env.NODE_ENV === "production") {
+    app.use(express.static("flutter/build/web"));
+  //}
   
 
   
 // all routes are in here
-app.use(routes);
+// app.use(routes);
 
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
